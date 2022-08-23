40e3951a90ee8005a96d1f17a8279d500c8dba25
AAYUSH VERMA <106806750+aayush-004v@users.noreply.github.com>
2022-08-23 11:54:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import React, {Component,useState} from "react";
+import '../styles/App.css';
+class App extends Component {
+    constructor(props) {
+    super(props);
+    this.state={
+      display: false
+    };
+   this.changePara=this.changePara.bind(this)	    
+	    
+  };
+  changePara(){
+    this.setState({display:true})
+  }
+  
 
-function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
+    render() {
+    	return(
+    		<div id="main">
+		<button id="click" onClick={this.changePara}>Click</button>
+		{this.state.display===true?<p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:''}
+    
+    		</div>
+    	);
+    }
 }
 
 
